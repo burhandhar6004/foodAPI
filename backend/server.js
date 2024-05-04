80a4c4cc5777543073697decd5b8d4a13b964e25
burhandhar6004 <burhaneducation6004@gmail.com>
2024-05-04 18:32:37 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 require("dotenv").config()
 const app = express();
-
+const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.get("/app", (req,res)=>{
   res.send("Food Court")
 })
-
 app.use("/app", require("./routers/foodRouters") );
 
 
